8be66a2cb10a11cbd534bddd177ab096e1966a19
Aleksey Lovchikov <svdmusic@gmail.com>
2016-10-12 21:40:29 +0300

    add SubMenu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,23 +3,28 @@ import React, { Component } from 'react';
 // style
 require('./Menu.css');
 
+// components
+import SubMenu from './SubMenu.jsx';
+
 export default class Menu extends Component {
     render() {
         return (
             <div className="menu">
                 <ul>
+                    {/* must be a component */}
                     <li>
                         <i className="fa fa-user" aria-hidden="true"></i> Publishers <span className="menu-count"><span className="menu-num">12</span> <i className="fa fa-angle-down" aria-hidden="true"></i></span>
+                        <SubMenu visible={true} />
                     </li>
                     <li>My Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
                     <li>Advertisers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
                     <li>My Advertisers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
-                    <li>Publishers <span>12</span></li>
-                    <li>Publishers <span>12</span></li>
-                    <li>Publishers <span>12</span></li>
-                    <li>Publishers <span>12</span></li>
-                    <li>Publishers <span>12</span></li>
-                    <li>Publishers <span>12</span></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
+                    <li>Publishers <i className="fa fa-angle-down" aria-hidden="true"></i></li>
                 </ul>
             </div>
         );
---
+++ src/components/SubMenu.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+require('./SubMenu.css');
+
+export default class SubMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {visible: this.props.visible};
+    }
+
+    render() {
+        const { visible } = this.state;
+        if (visible) {
+            return (
+                <ul className="menu-inside">
+                    <li><i className="fa fa-columns" aria-hidden="true"></i> Sites</li>
+                    <li><i className="fa fa-line-chart" aria-hidden="true"></i> Statistic</li>
+                    <li><i className="fa fa-usd" aria-hidden="true"></i> Payments</li>
+                </ul>
+            );
+        } else {
+            return <div></div>;
+        }
+    }
+}
